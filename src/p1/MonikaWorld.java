80f6b266a1640fa31ea68008f9646957b2b92ae4
monika rajoriya <monika.rajoriya1@gmail.com>
2018-12-19 22:34:57 +0530

    wants to go paris

--- src/p1/MonikaWorld.java
+++ src/p1/MonikaWorld.java
@@ -7,6 +7,9 @@ public class MonikaWorld {
 		System.out.println("It is Monika world!");
 		System.out.println("Monika Likes chole bhature");
 		System.out.println("Monika wants jacket");
+		System.out.println("Monika wants to go Paris");
+		
+		
 		
 	}
 
