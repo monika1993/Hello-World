a99cd9241dbdfa2280991b24897d8b0517baa24e
monika rajoriya <monika.rajoriya1@gmail.com>
2025-07-18 11:18:38 +0100

    Update MonikaWorld.java

--- src/p1/MonikaWorld.java
+++ src/p1/MonikaWorld.java
@@ -7,7 +7,7 @@ public class MonikaWorld {
 		System.out.println("It is Monika world!");
 		System.out.println("Monika Likes chole bhature");
 		System.out.println("Monika wants jacket");
-		System.out.println("Monika wants to go Paris");
+		System.out.println("Monika wants to go to XYZ Place");
 		
 		
 		
